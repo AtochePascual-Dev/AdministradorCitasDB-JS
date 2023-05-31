5956c319adf5be4081d0c041df72f6eeaace5674
Atoche Pascual <jmatochepascual@gmail.com>
2023-05-31 09:49:28 -1000

    [ADD] variables

--- js/variables.js
+++ js/variables.js
@@ -5,5 +5,13 @@ export const inputFecha = document.querySelector('#fecha');
 export const inputHora = document.querySelector('#hora');
 export const inputSintomas = document.querySelector('#sintomas');
 export const formulario = document.querySelector('#nueva-cita');
+export let citaDatos = {
+  mascota: '',
+  propietario: '',
+  telefono: '',
+  fecha: '',
+  hora: '',
+  sintomas: '',
+};
 
 
